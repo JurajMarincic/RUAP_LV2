b875f6e246d7726a9b2f657483f2939654be9ca2
JurajMarincic <juraj.marincic1@gmail.com>
2023-01-30 21:06:58 +0100

    Task 4 - Extracting Functionality into a Service Layer

--- Begin1/ContactController1/Controllers/ContactController.cs
+++ Begin1/ContactController1/Controllers/ContactController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ContactController1.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContactController1.Services;
 
 namespace ContactController1.Controllers
 {
@@ -11,13 +13,28 @@ namespace ContactController1.Controllers
     [ApiController]
     public class ContactController : ControllerBase
     {
-        public string[] Get()
+        public Contact[] Get()
         {
-            return new string[]
+
+            return new Contact[]
             {
-        "Hello",
-        "World"
+        new Contact
+        {
+            Id = 1,
+            Name = "Glenn Block"
+        },
+        new Contact
+        {
+            Id = 2,
+            Name = "Dan Roth"
+        }
             };
+        }
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
         }
     }
 }
